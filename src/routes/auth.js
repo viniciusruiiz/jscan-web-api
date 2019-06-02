302adaa1bbb7aa1d1c94ac9d795bf89b9359fb78
Vinicius Ruiz <viniciusruiiz>
2019-06-02 13:04:45 -0300

    arrumando auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
     if (!!req.body.email && !!req.body.senha) {
-        console.log(!!req.body.email)
         var email = req.body.email, senha = req.body.senha;
         global.conn.request()
             .query(`
@@ -25,9 +24,8 @@ LEFT JOIN TB_COMPUTADOR  C
     WHERE F.NMEMAIL = '${email}'`)
             .then(user => {
                 if (user.recordset.length > 0) {
-                    console.log(senha, user.recordset[0].nmSenha, hashCode(senha));
-                    if (user.recordset[0].nmSenha == hashCode(senha)) {
-                        let payload = { id: user.recordset[0].idFuncionario };
+                    if (user.recordset[0].NMSENHA == hashCode(senha)) {
+                        let payload = { id: user.recordset[0].IDFUNCIONARIO };
                         let token = jwt.sign(payload, options.secretOrKey);
                         user.recordset[0].token = token;
                         user.recordset[0].message = "Autenticado com Sucesso!";
@@ -73,7 +71,6 @@ LEFT JOIN TB_COMPUTADOR  C
                                 ]
                                 break;
                         }
-                        console.log(user.recordset[0]);
                         res.json(user.recordset[0]);
                     } else {
                         res.json({ "message": "senha incorreta" }).status(401);
